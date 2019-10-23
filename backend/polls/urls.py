ed7f7dc481132ac257636e94778c7ad87e5856d3
caleb.teo <teoc@tcd.ie>
2019-10-23 14:07:09 +0100

    More playing around

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.testindex, name='testindex'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.result, name='results'),
+    path('<int:question_id>/votes/', views.vote, name='votes'),
 ]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -2,11 +2,32 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def testindex(request):
     return HttpResponse("Directing you here now...")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
+
