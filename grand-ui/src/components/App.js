2221ad5fda31e1051cfb371808782d02335c146f
skarode96 <skarode96@gmail.com>
2019-10-29 02:12:50 +0000

    Adding Router Support

---
+++ grand-ui/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import LeftDrawer from "./LeftDrawer/LeftDrawer.component";
+import MenuAppBar from "./MenuAppBar/MenuAppBar.component";
+
+
+function App() {
+  return (
+      <React.Fragment>
+      </React.Fragment>
+  );
+}
+
+export default App;
