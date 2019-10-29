a88a638298aaebfca494b2ea7555600970dc8833
Rohan Anand <thededlier@gmail.com>
2019-10-29 13:22:58 +0530

    user profile start

--- backend/grandbackend/models.py
+++ backend/grandbackend/models.py
@@ -5,3 +5,8 @@ class AppUser(models.Model):
     name = models.CharField(max_length = 128)
     username = models.CharField(max_length = 128)
     password = models.CharField(max_length = 128)
+
+class AppUserProfile(models.Model):
+    app_user = models.OneToOneField(AppUser, on_delete = models.CASCADE)
+    dob = models.DateField()
+    gender = models.IntegerField() # Male = 0, Female = 1, Other = 2
--- backend/grandbackend/serializers.py
+++ backend/grandbackend/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
-from grandbackend.models import AppUser
+from grandbackend.models import AppUser, AppUserProfile
 
+class AppUserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppUserProfile
+        fields = ['dob', 'gender']
 
 class AppUserSerializer(serializers.ModelSerializer):
+    appuserprofile = AppUserProfileSerializer(many=False, read_only=True)
+
     class Meta:
         model = AppUser
-        fields = ['id', 'name', 'username']
-        
+        fields = ['id', 'name', 'username', 'appuserprofile']
+
     def create(self, validated_data):
         return AppUser.objects.create(**validated_data)
 
