e30743ce85b188876caacfeab2d7b10bfcb74c0a
skarode96 <skarode96@gmail.com>
2019-10-30 17:26:48 +0000

    moving routing to app.js

--- grand-ui/src/components/App.js
+++ grand-ui/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import '../App.css';
-import LeftDrawer from "./LeftDrawer/LeftDrawer.component";
 import MenuAppBar from "./MenuAppBar/MenuAppBar.component";
+import history from "../history";
+import SignInComponent from "./SignIn/SignIn.component";
+import SignUp from "./SignUp/SignUp.component";
+import Dashboard from "./Dashboard/Dashboard.component";
+import {Router, Route} from 'react-router-dom';
 
 
 function App() {
   return (
       <React.Fragment>
+          <MenuAppBar/>
+          <Router history={history}>
+              <div>
+                  <Route exact path="/" component={SignInComponent} />
+                  <Route path="/signin" component={SignInComponent} />
+                  <Route path="/signup" component={SignUp} />
+                  <Route path="/dashboard" component={Dashboard} />
+              </div>
+          </Router>
       </React.Fragment>
   );
 }
--- grand-ui/src/index.js
+++ grand-ui/src/index.js
@@ -3,30 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import {Router as Router, Route} from 'react-router-dom';
 
-import SignInComponent from "./components/SignIn/SignIn.component";
-import SignUp from "./components/SignUp/SignUp.component";
-import MenuAppBar from "./components/MenuAppBar/MenuAppBar.component";
-import history from "./history";
-import Dashboard from "./components/Dashboard/Dashboard.component";
-
-
-const routing = (
-    <React.Fragment>
-    <MenuAppBar/>
-    <Router history={history}>
-        <div>
-            <Route exact path="/" component={SignInComponent} />
-            <Route path="/signin" component={SignInComponent} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/dashboard" component={Dashboard} />
-        </div>
-    </Router>
-    </React.Fragment>
-);
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
