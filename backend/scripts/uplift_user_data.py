ec6bdea94c7549fc93a3bf5844d9a28cbef0ef1b
Rohan Anand <thededlier@gmail.com>
2019-11-20 11:03:21 +0530

    uplift script

--- backend/scripts/uplift_user_data.py
+++ backend/scripts/uplift_user_data.py
@@ -8,5 +8,15 @@ import sqlalchemy as db
 user_data = pickle.load(open('user_data.pkl', 'rb'))
 user_profile_data = pickle.load(open('user_profile_data.pkl', 'rb'))
 user_food_history_data = pickle.load(open("user_food_history_data.pkl", 'rb'))
+user_profile_data['dob'] = '1994-10-02'
+user_profile_data.rename(columns={'id': 'app_user_id'}, inplace=True)
 
+print(user_data.head())
 print(user_profile_data.head())
+print(user_food_history_data.head())
+
+print("Starting sql uplift")
+engine = db.create_engine('mysql+mysqldb://grand_app:grand_app@localhost:3306/grand_app')
+user_data.to_sql('grandbackend_appuser', engine, if_exists='append', index=False)
+user_profile_data.to_sql('grandbackend_appuserprofile', engine, if_exists='append', index=False)
+user_food_history_data.to_sql('grandbackend_userfoodhistory', engine, if_exists='replace', index=False)
